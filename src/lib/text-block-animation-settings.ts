fa6050192a5eb8198a74b807fb2a759fe8342e67
Tim Shaw <tim@deskpass.com>
2026-01-22 11:28:39 -0800

    Animation timing adjustments
    Signed-off-by: Tim Shaw <tim@deskpass.com>

--- src/lib/text-block-animation-settings.ts
+++ src/lib/text-block-animation-settings.ts
@@ -1,9 +1,9 @@
 export const TEXT_BLOCK_ANIMATION = {
-  duration: 0.2,
-  ease: "easeInOut" as const,
+  duration: 0.3,
+  ease: "backOut" as const,
 } as const
 
 export const TEXT_BLOCK_FADE = {
-  duration: 0.15,
-  ease: "easeInOut" as const,
+  duration: 0.3,
+  ease: "backOut" as const,
 } as const
