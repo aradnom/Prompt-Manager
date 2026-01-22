a592045e4230d4c877d96d4c8d707d504b87e812
Tim Shaw <tim@deskpass.com>
2026-01-21 16:08:03 -0800

    Add cursor-pointer to dialog close X icon in src/components/ui/dialog.tsx so the close control shows pointer cursor on hover for clearer UX
    Signed-off-by: Tim Shaw <tim@deskpass.com>

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -43,7 +43,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-magenta-medium focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-cyan-dark data-[state=open]:text-cyan-medium">
-        <X className="h-4 w-4" />
+        <X className="h-4 w-4 cursor-pointer" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
